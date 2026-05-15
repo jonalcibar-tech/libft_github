40c7dca316319796f10377a5175ba3109eb2345e
jalcibar <jonalcibar@gmail.com>
2026-05-15 16:57:31 +0200

    Add files via upload

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,10 +6,11 @@
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 10:43:59 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/05/15 13:06:45 by jalcibar         ###   ########.fr       */
+/*   Updated: 2026/05/15 16:55:29 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//test
 #include <stddef.h>
 
 void *ft_memchr(const void *s, int c, size_t n)
@@ -18,10 +19,10 @@ void *ft_memchr(const void *s, int c, size_t n)
 	char	*tmp_s;
 	
 	i = 0;
-	tmp_s = &s;
+	tmp_s = (char*)s;
 	while ((i < n) && tmp_s[i] && tmp_s[i] != c)
 		i++;
-	return (&s[i]);
+	return ((void*)tmp_s[i]);
 }
 #include	<stddef.h>
 #include	<string.h>
@@ -35,8 +36,8 @@ int	main(void)
 
 	c = 'd';
 	n = 5;
-	printf("%s %c %zu %s\n", s, c, n, (char *)memchr(s, c, n));
-	printf("%s %c %zu %s\n", s, c, n, (char *)ft_memchr(s, c, n));
+	printf("%s %c %zu %s\n", s, c, n, (char*)memchr(s, c, n));
+	printf("%s %c %zu %s\n", s, c, n, (char*)ft_memchr(s, c, n));
 	return (0);
 }
 

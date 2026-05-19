5b922987f2bf6b9f112ba1a4407c892644a36ec5
jalcibar <jonalcibar@gmail.com>
2026-05-19 14:40:48 +0200

    Add files via upload

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,26 +6,30 @@
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 08:45:10 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/05/19 09:27:34 by jalcibar         ###   ########.fr       */
+/*   Updated: 2026/05/19 14:38:54 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	<stddef.h>
-#include	<stdio.h>
-void *ft_memchr(const void *s, int c, size_t n)
+//#include	<stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
+	unsigned char	c_tmp;
 	unsigned char	*tmp_s;
-	
+
+	c_tmp = (unsigned char)c;
+	tmp_s = (unsigned char *)s;
 	i = 0;
-	*tmp_s = (unsigned char *)s;
-	while ((i < n) && tmp_s[i] != c)
+	while (i < n)
+	{
+		if (tmp_s[i] == c_tmp)
+			return (&tmp_s[i]);
 		i++;
-	if (tmp_s[i] == (unsigned char*)c)
-		return (&tmp_s[i]);
-	else
-		return (NULL);
+	}
+	return (NULL);
 }
+/*
 #include	<stddef.h>
 #include	<string.h>
 #include	<stdio.h>
@@ -36,13 +40,13 @@ int	main(void)
 	int		c;
 	size_t	n;
 
-	c = 'W';
-	n = 8;
+	c = 'r';
+	n = 20;
 	printf("%s %c %zu %s\n", s, c, n, (char*)memchr(s, c, n));
 	printf("%s %c %zu %s\n", s, c, n, (char*)ft_memchr(s, c, n));
 	return (0);
 }
-
+*/
 /*
 void *memchr(const void *s, int c, size_t n);
 The  memchr()  function  scans  the  initial n bytes of the memory area

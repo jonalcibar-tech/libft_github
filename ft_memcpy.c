655f848ecae05e147c34f30a1da1b11f13b0ac8d
jalcibar <jonalcibar@gmail.com>
2026-05-25 20:07:42 +0000

    2505 L PM Luna

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,7 +14,21 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	count;
+	size_t		i;
+	const char*	s;
+	char *		d;
+
+	*s = (const char *)src;
+	*0
+	{
+		/* data */
+	};
+	
+
+	{
+		/* code */
+	}
+	
 
 	count = 0;
 	while (count <= n)

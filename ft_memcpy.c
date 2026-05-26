4218a1c4ff0e13cb2a1efae53348d0b26e373fe8
jalcibar <jonalcibar@gmail.com>
2026-05-26 17:08:31 +0200

    Add files via upload

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,57 +6,62 @@
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 11:36:29 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/05/20 12:51:50 by jalcibar         ###   ########.fr       */
+/*   Updated: 2026/05/26 17:00:00 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
 #include <stddef.h>
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t		i;
-	const char*	s;
-	char *		d;
+	const char *		s;
+	unsigned char *		d;
+	size_t				i;
 
-	*s = (const char *)src;
-	*0
-	{
-		/* data */
-	};
-	
+	s = src;
+	d = dest;
 
-	{
-		/* code */
-	}
-	
-
-	count = 0;
-	while (count <= n)
+	if (i == 0 || d == '\0')
+		return (d);
+	i = 0;
+	while (i <= n, i++)
+		d[i] == s[i];
+	return (*d);
+}
+*/
+/* LO QUE PUSO OLIVER
+	while (i <= n)
 	{
 		((unsigned char *)dest)[count] = ((unsigned char *)src)[count];
 		count++;
 	}
 	return (dest);
 }
-/*
+*/
+
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
-	char	src_string[]  = "see you world";
-	char	dest_string[] = "lola";
+	char	src1[]  = "see you world";
+	char	dest1[20] = "lola"; //string long enough not to end in core dump
+	size_t	n;
+	char*	src2;
+	char*	dest2;
 
-	printf("%s\n", src_string);
-	printf("%s\n", dest_string);
-	printf("%p\n", &src_string);
-	printf("%p\n", &dest_string);
-	printf("%p\n", ft_memcpy(dest_string, src_string, strlen(src_string)));
-	printf("%s\n", src_string);
-	printf("%s\n", dest_string);
+	n = strlen(src1) + 1;
+	strcpy(src2, src1);
+	strcpy(dest2, dest1);
+
+	printf("%s    %s    %ld\n", src1, dest1, n);
+	memcpy(dest1, src1, n);
+	printf("%s    %s    %ld\n", src1, dest1, n);
+	
 	return (0);
 }
-*/
+
 /*
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 The  memcpy()  function  copies  n bytes from memory area src to memory

e6389a1e90a8194005b7fce09ecf237986598bb2
jalcibar <jonalcibar@gmail.com>
2026-06-10 13:32:48 +0200

    Add files via upload

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,28 +6,36 @@
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 11:36:29 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/05/29 17:28:30 by jalcibar         ###   ########.fr       */
+/*   Updated: 2026/06/10 11:35:29 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stddef.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	count;
-	char	*temporary_array[n];
+	size_t	c;
+	char	*src_tmp;
+	char	*dest_tmp;
 
-	count = 0;
-	while (count <= n)
+	if (dest == NULL && src == NULL)
+		return (dest);
+	dest_tmp = (char *) dest;
+	src_tmp = (char *) src;
+	if (dest > src)
 	{
-		((char *)temporary_array)[count] = ((const char *)src)[count];
-		count++;
+		while (n-- > 0)
+			dest_tmp[n] = src_tmp[n];
 	}
-	count = 0;
-	while (count <= n)
+	else
 	{
-		((char *)dest)[count] = ((char *)temporary_array)[count];
-		count++;
+		c = 0;
+		while (c < n)
+		{
+			dest_tmp[c] = src_tmp[c];
+			c++;
+		}
 	}
 	return (dest);
 }
@@ -36,24 +44,30 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 #include <string.h>
 int main(void)
 {
-	const char	src1[]  = "1234";
-	char		dest1[] = "abcdef";
-	size_t	n;
-	const char	src2[]  = "1234";
-	char	dest2[] = "abcdef"; 
+char str1[] = "HOLA MUNDO";
+char str2[] = "HOLA MUNDO";
+char str3[] = "HOLA MUNDO";
 
-	// strcpy (src2, src1);
-	// strcpy (dest2, dest1);
-	n =  10;
+printf("Antes ft_memmove (sin overlap):\n");
+printf("str1: %s\n", str1);
 
-	printf("%s    %s    %ld\n\n", src1, dest1, n);
-	memmove(dest1, src1, n);
-	printf("%s    %s-\n", src1, dest1);
+ft_memmove(str1 + 5, str1, 2);
+str1[10] = '\0';
 
-	ft_memmove(dest2, src2, n);
-	printf("%s    %s-\n", src2, dest2);
-	
-	return (0);
+printf("Después ft_memmove (con overlap):\n");
+printf("str1: %s\n\n", str1);
+
+printf("Comparación con memmove estándar:\n");
+memmove(str2 + 5, str2, 2);
+str2[10] = '\0';
+printf("str2 (memmove): %s\n", str2);
+
+printf("\nCaso simple sin overlap:\n");
+ft_memmove(str3, "ABCDE", 2);
+str3[5] = '\0';
+printf("str3: %s\n", str3);
+
+return (0);
 }
 */
 /*
@@ -64,4 +78,5 @@ area dest.  The memory areas may overlap: copying takes place as though
 the  bytes in src are first copied into a temporary array that does not
 overlap src or dest, and the bytes are then copied from  the  temporary
 array to dest.
+The memmove() function returns a pointer to dest.
 */
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,48 +6,51 @@
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 10:51:42 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/06/01 11:14:04 by jalcibar         ###   ########.fr       */
+/*   Updated: 2026/06/10 13:29:03 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include "libft.h"
 #include <stddef.h>
-/*
-int	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	count;
 
-	count = 0;
-	if (size != 0)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	dst_len;
+	size_t	src_len;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+
+	if (size == 0)
+		return (src_len);
+	if ((src_len + 1) < size) //+1 para que haya sitio para \0 final
+		ft_memcpy(dst, src, src_len + 1);
+	else 
 	{
-		while ((count <= size - 1) && (((const char *) src)[count] != '\0'))
-		{
-			((char *) dst) [count] = ((const char*) src) [count];
-			count ++;
-		}
-		((char *) dst) [count] = '\0';
+		ft_memcpy(dst, src, src_len + 1);
+		dst[dst_len+1] = '\0';
 	}
-	return (*src);
+	return (src_len);
 }
-*/
-/*
 #include <stdio.h>
 #include <bsd/string.h>
 
 int	main(void)
 {
-	const char	src_string[] = "hello";
-	char		dst_string[] = "goodbye";
+	const char	src1[] = "HOLA MUNDO";
+	char		dst1[] = "";
+	const char	src2[] = "HOLA MUNDO";
+	char		dst2[] = "";
+	size_t		n;
 
-	printf("%p   %s\n", &src_string, src_string);
-	printf("%p   %s\n\n", &dst_string, dst_string);
-	ft_strlcpy(dst_string, src_string, 15);
-	printf("%p   %s\n", &src_string, src_string);
-	printf("%p   %s\n", &dst_string, dst_string);
-	return 0;
+	n = 5;
+	printf("%s - %s %zu\n", dst1, src1, n);
+	//printf("%zu %s\n", strlcpy(dst1, src1, n), dst1);
+	printf("%zu %s\n", ft_strlcpy(dst2, src2, n), dst2);
+	return (0);
 }
-*/	
 /*
-strlcpy(char *dst, const char *src, size_t size);
+size_t strlcpy(char *dst, const char *src, size_t size);
 
 The strlcpy() function copies up to size - 1 characters from the NUL-terminated
 string src to dst, NUL-terminating the result.

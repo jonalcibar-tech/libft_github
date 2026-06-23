83b351a02a628c90658a288d8116e8e171a2ccd0
jalcibar <jonalcibar@gmail.com>
2026-06-23 12:50:34 +0200

    Add files via upload

--- ft_strlcat_old.c
+++ ft_strlcat_old.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat_old.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/15 16:08:18 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/06/23 12:39:58 by jalcibar         ###   ########.fr       */
+/*   Updated: 2026/06/22 17:34:11 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 #include "stdio.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
-{	
-	size_t    dst_len;
-	size_t    src_len;
-    
+{
+	size_t	dst_len;
+	size_t	src_len;
+
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	if (dst_len >= size)
-	dst_len = size;
-	if (dst_len == size)
-    	return (size + src_len);
-	if (src_len < size - dst_len)
-		ft_memcpy(dst + dst_len, src, src_len + 1);
+	if (src == NULL || size == dst_len || size == 0)
+		return (dst_len + src_len);
+	if (dst_len < (size - 1))
+	{
+		ft_memcpy(dst + dst_len, src, size - dst_len);
+		dst[size - 1] = '\0';
+	}
 	else
 	{
-    	ft_memcpy(dst + dst_len, src, size - dst_len - 1);
-		dst[size - 1] = '\0';
+		return (size + src_len);
 	}
 	return (dst_len + src_len);
 }
-
+/*
 #include <stdio.h>
 #include <bsd/string.h>
 
@@ -51,7 +51,7 @@ int	main(void)
 	//printf("%zu %s-\n", strlcat(dst, src, n), dst);
 	return (0);
 }
-
+*/
 /*
 strlcat(char *dst, const char *src, size_t size);
 
@@ -61,6 +61,4 @@ nating the result.  The initial character of the string(src) overwrites the
 Null-character present at the end of the string(dest).
 
 returns strlcat() the initial length of dst plus the length of src
-strlcat no devuelve cuánto ha copiado sino cuánto habría medido el
-resultado ideal”.
 */

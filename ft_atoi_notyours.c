be4d98934be1732c821e24b779a4302b3a1504dd
jalcibar <jonalcibar@gmail.com>
2026-07-16 13:28:51 +0200

    Add files via upload

---
+++ ft_atoi_notyours.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/16 12:59:12 by jalcibar          #+#    #+#             */
+/*   Updated: 2026/07/16 13:18:43 by jalcibar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int    ft_isspace(int c)
+{
+    if (c == 9 || c == 10 || c == 11 || c == 12 || c == 13 || c == 32)
+        return (1);
+    return (0);
+}
+
+int    ft_atoi(const char *str)
+{
+    int    result;
+    int    sign;
+    int    i;
+    
+    result = 0;
+    sign = 1;
+    i = 0;
+    while (ft_isspace(str[i]))
+        i++;
+    if (str[i] == '+' && str[i + 1] != '-')
+        i++;
+    if (str[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    while (str[i] && str[i] >= 48 && str[i] <= 57)
+    {
+        result *= 10;
+	result += str[i] - 48;
+	i++;
+    }
+    result *= sign;
+    return (result);
+}
+/*
+int main(void)
+{
+	const char	nptr[] = "--27";
+
+	printf("%d\n", atoi(nptr));
+	printf("%d", ft_atoi(nptr));
+	return(0);
+}
+*/

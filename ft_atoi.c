80d11438cbd81a0547c0d53fcdbf3aec67b267f7
jalcibar <jonalcibar@gmail.com>
2026-07-20 16:31:21 +0200

    Add files via upload

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,55 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/03 18:32:27 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/07/15 16:59:14 by jalcibar         ###   ########.fr       */
+/*   Created: 2026/07/16 15:00:35 by jalcibar          #+#    #+#             */
+/*   Updated: 2026/07/20 16:12:12 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int    ft_atoi(const char *nptr)
+int	ft_isspace(int c)
 {
-    size_t	i;
-    long    value;
-    char    charact;
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	size_t	i;
+	int		value;
 	int		sign;
 
-    i = 0;
-    value = 0;
+	i = 0;
+	value = 0;
 	sign = 1;
-    while (i <= ft_strlen(nptr) - 1)
-    {
-        charact = nptr[i];
-        if ((charact >= 0 && charact <= 32) && charact != '\n')
-            value = 0;
-        else if (ft_isdigit(charact))
-        	value = value * 10 + (charact - 48);
-        else if (charact == '+')
-        {}
-        else if (charact == '-')
-			sign = -sign;
+	while (ft_isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '+' && nptr[i + 1] != '-')
+		i++;
+	if (nptr[i] == '-')
+	{
+		sign = -1;
 		i++;
 	}
-	value = value * sign;
-    if (value >= 2147483648)
-        return (2147483648 - 1);
-    if (value < -2147483648)
-        return (-2147483648);
-    return (value);
+	while (ft_isdigit(nptr[i]))
+	{
+		value = value * 10 + nptr[i] - 48;
+		i++;
+	}
+	return (value * sign);
 }
 /*
-int    main(void)
+int	main(void)
 {
-    const char    nptr[] = "-4";
+	const char	nptr[] = "4242";
 
-    printf("%d\n", atoi(nptr));
-    printf("%d", ft_atoi(nptr));
-    return(0);
+	printf("%d\n", atoi(nptr));
+	printf("%d", ft_atoi(nptr));
+	return(0);
 }
-*/
-/*
-The  atoi() function converts the initial portion of the string pointed
-to by nptr to int.  The behavior is the same as strtol(nptr, NULL, 10);
-except that atoi() does not detect errors.
 */

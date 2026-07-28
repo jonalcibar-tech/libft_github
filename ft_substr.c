411c1fdab4e27b6566057760c3830fe94c787c3e
jalcibar <jonalcibar@gmail.com>
2026-07-28 13:17:19 +0200

    Add files via upload

--- ft_substr.c
+++ ft_substr.c
@@ -5,34 +5,51 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/27 15:53:01 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/07/27 17:02:34 by jalcibar         ###   ########.fr       */
+/*   Created: 2026/07/28 12:21:45 by jalcibar          #+#    #+#             */
+/*   Updated: 2026/07/28 13:02:17 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	"libft.h"
+#include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*result;
-	
-	if ((start + len) > (ft_strlen(s)+1))
-		return ("");
-	result = malloc(start + len);
+	size_t	slen;
+	size_t	chnum;
+
+	chnum = 0;
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start >= slen)
+		chnum = 0;
+	else if (len < slen - start)
+		chnum = len;
+	else
+		chnum = slen - start;
+	result = malloc(chnum + 1);
 	if (!result)
 		return (NULL);
-	ft_memcpy(&start, &s, len);
+	ft_memcpy(result, s + start, chnum);
+	result[chnum] = '\0';
 	return (result);
-
 }
-
+/*
 int	main(void)
 {
-	char const		*s = "Hola que tal";
+	char const		*s = NULL;
 	unsigned int	start;
 	size_t			len;
 
-	start = 5;
+	start = 15;
 	len = 5;
-	printf("%s\n", ft_substr(s, start, len));
+	printf("%s", ft_substr(s, start, len));
 }
+*/
+/*
+Reserva memoria (con malloc(3)) y devuelve una
+subcadena de caracteres de la cadena ‘s’.
+La subcadena comienza en el índice ‘start ’ y tiene
+una longitud máxima ‘len ’.
+*/

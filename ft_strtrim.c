2faedc21349851279e59fb2ff8fd1bb7db1320ea
jalcibar <jonalcibar@gmail.com>
2026-08-14 12:32:33 +0200

    Add files via upload

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,13 @@
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 08:45:05 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/08/10 15:38:17 by jalcibar         ###   ########.fr       */
+/*   Updated: 2026/08/14 12:28:38 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	size_t ft_start(char const *s1, char const *set)
+static	size_t	ft_start(char const *s1, char const *set)
 {
 	size_t	begin;
 	size_t	iset;
@@ -22,22 +22,23 @@ static	size_t ft_start(char const *s1, char const *set)
 	is1 = 0;
 	begin = 0;
 	while (s1[is1])
-	{	
+	{
 		while (set [iset])
 		{
 			if (s1[is1] == set[iset])
-				{
-					begin++;
-					iset = 0;
-					break;
-				}
-		iset++;
+			{
+				begin++;
+				iset = 0;
+				break ;
+			}
+			iset++;
 		}
-	is1++;
+		is1++;
 	}
-	return(begin);
+	return (begin);
 }
-static	size_t ft_trimend(char const *s1, char const *set)
+
+static	size_t	ft_end(char const *s1, char const *set)
 {
 	size_t	end;
 	size_t	iset;
@@ -45,33 +46,36 @@ static	size_t ft_trimend(char const *s1, char const *set)
 
 	iset = 0;
 	is1 = ft_strlen(s1) - 1;
-	end = ft_strlen(s1) - 1; 
+	end = ft_strlen(s1) - 1;
 	while (set[iset])
-	{	
+	{
 		while (s1[is1])
 		{
 			if (s1[is1] != set[iset])
-				break;
-		end--;
-		is1--;
+				break ;
+			end--;
+			is1--;
 		}
-	iset++;
+		iset++;
 	}
-	return(end + 1);
+	return (end + 1);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	begin;
+	size_t	end;
 	size_t	len;
-	size_t	trimend;
 
 	if (!s1 || !set)
 		return (NULL);
+	if (ft_strlen(s1) == 0)
+		return (ft_strdup(""));
 	begin = ft_start(s1, set);
-	trimend = ft_trimend(s1, set);
-	len = trimend - begin;
-	printf("%zu %zu %zu\n", begin, trimend, len);
+	end = ft_end(s1, set);
+	len = end - begin;
+	if (len == 0)
+		return (ft_strdup(""));
 	return (ft_substr(s1, begin, len));
 }
 /*

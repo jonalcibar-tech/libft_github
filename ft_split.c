8cd93cc2dba1d2fb3962b469f231f108331497a1
jalcibar <jonalcibar@gmail.com>
2026-08-18 16:47:09 +0200

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,10 +6,95 @@
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 09:49:56 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/08/18 11:29:45 by jalcibar         ###   ########.fr       */
+/*   Updated: 2026/08/18 16:44:44 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include	"libft.h"
+
+static	size_t	ft_start(char const *s1, char const *set)
+{
+	size_t	begin;
+	size_t	iset;
+	size_t	is1;
+
+	iset = 0;
+	is1 = 0;
+	begin = 0;
+	while (s1[is1])
+	{
+		while (set [iset])
+		{
+			if (s1[is1] == set[iset])
+			{
+				begin++;
+				iset = 0;
+				break ;
+			}
+			iset++;
+		}
+		is1++;
+	}
+	return (begin);
+}
+
+static	size_t	ft_end(char const *s1, char const *set)
+{
+	size_t	end;
+	size_t	iset;
+	size_t	is1;
+
+	iset = 0;
+	is1 = ft_strlen(s1) - 1;
+	end = ft_strlen(s1) - 1;
+	while (set[iset])
+	{
+		while (s1[is1])
+		{
+			if (s1[is1] != set[iset])
+				break ;
+			end--;
+			is1--;
+		}
+		iset++;
+	}
+	return (end + 1);
+}
+
+
+static size_t ft_wordsnr(char const *s, char c)
+// funciona contando palabras pero no las mide
+{
+	size_t	countwords;
+	size_t	i;
+
+	if (s == NULL)
+		return(0);
+	countwords = 0 + (s[0] != 0);
+	i = 0;
+	while (s[i])
+		countwords+= (s[i++] == c);
+	return(countwords);
+}
+
+
+
+char	**ft_split(char const *s, char c);
+{
+	**matrix = ft_calloc((wordsnr(s, c) + 1), sizeof(char *));
+	
+}
+*/
+int	main(void)
+{
+	const char *s = NULL;
+	char  c;
+
+	c = ' ';
+	printf("%zu", ft_wordsnr(s, c));
+	//printf("%p?", ft_split(s, c));
+	return(0);
+}
 
 /*
 s: La cadena que se va a dividir.

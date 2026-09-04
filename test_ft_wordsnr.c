c5b50cea2008e68e8e7d4111161b98b54db9654b
jalcibar <jonalcibar@gmail.com>
2026-09-04 13:44:23 +0200

    Add files via upload

--- test_ft_wordsnr.c
+++ test_ft_wordsnr.c
@@ -2,14 +2,35 @@
 size_t ft_wordsnr(char const *s, char c)
 // funciona contando palabras pero no las mide
 {
-	size_t	iwords;
-	size_t	i;
+    size_t iwords;
+    size_t i;
+    size_t inword;
 
-	if (s == NULL)
-		return(0);
-	iwords = 0;
-	i = 0;
-	while (s[i])
-		iwords+= (s[i++] == c);
-	return(iwords);
+    if (s == NULL)
+        return(0);
+    iwords = 0;
+    i = 0;
+    inword = 1;
+    while (s[i])
+    {
+        if(s[i] != c && inword == 0)
+        {
+            iwords++;
+            inword = 1;
+        }
+        else if (s[i] == c)
+          inword = 0;
+        i++;
+    }
+    return(iwords);
+}
+int	main(void)
+{
+	const char *s = ",,,Hola,,,mi,mundo,,,";
+	char  c;
+
+	c = ',';
+	printf("%zu", ft_wordsnr(s, c));
+	//printf("%p?", ft_split(s, c));
+	return(0);
 }

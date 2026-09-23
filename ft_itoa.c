d48aae0b8003b799345111f9e31c25fcc1ab2b3e
jalcibar <jonalcibar@gmail.com>
2026-09-23 18:50:14 +0200

    Add files via upload

---
+++ ft_itoa.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/23 18:33:50 by jalcibar          #+#    #+#             */
+/*   Updated: 2026/09/23 18:48:59 by jalcibar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"libft.h"
+
+char *ft_itoa(int n)
+{
+	char	*numbers;
+	
+	
+	*numbers = malloc()
+}
+int	main(void)
+{
+	int	n;
+	n =	5;
+
+	printf("%s", ft_itoa(n))
+}
+
+/*
+char *ft_itoa(int n);
+Reserva memoria (utilizando malloc(3)) y devuelve
+una cadena que represente el valor del número
+entero recibido como argumento. Debe ser capaz de
+manejar números negativos.
+*/

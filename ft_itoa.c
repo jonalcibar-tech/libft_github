b7c3ee5f5e7c2a26b6ff86825bf4196358d9d9c4
jalcibar <jonalcibar@gmail.com>
2026-09-25 13:33:09 +0200

    Add files via upload

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,25 +6,28 @@
 /*   By: jalcibar <jalcibar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/23 18:33:50 by jalcibar          #+#    #+#             */
-/*   Updated: 2026/09/23 18:48:59 by jalcibar         ###   ########.fr       */
+/*   Updated: 2026/09/25 13:19:38 by jalcibar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
 
-char *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-	char	*numbers;
+	//cuenta nº caracteres de string considera 0s y '-', no hay funcion que lo haga
 	
-	
-	*numbers = malloc()
+
+	//reserva memoria para ello + NULL y devuelve NULL si no lo logra
+	//copia caracter por caracter ¿dcha a izq?
+	//libera la memoria.
 }
+
 int	main(void)
 {
 	int	n;
 	n =	5;
 
-	printf("%s", ft_itoa(n))
+	printf("%s", ft_itoa(n));
 }
 
 /*
